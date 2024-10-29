4988f7eb1384a347eaf5d73a097ad974c7a0d222
Mariyan Dimitrov <mariyan.dimitrov@canonical.com>
2024-10-29 15:29:35 +0200

    fix(VPC): Adjust terraform options

--- test/utils/aws.go
+++ test/utils/aws.go
@@ -116,22 +116,7 @@ func CreateVPC(t TestingT, region, environment, projectName string) *terraform.O
 		Vars: map[string]interface{}{
 			"environment":  environment,
 			"project_name": projectName,
-			"cidr_block":   "10.0.0.0/16",
-			"azs": []string{
-				region + "a",
-				region + "b",
-				region + "c",
-			},
-			"private_subnets": []string{
-				"10.0.1.0/24",
-				"10.0.2.0/24",
-				"10.0.3.0/24",
-			},
-			"public_subnets": []string{
-				"10.0.101.0/24",
-				"10.0.102.0/24",
-				"10.0.103.0/24",
-			},
+			"vpc_cidr":     "10.0.0.0/16",
 		},
 		EnvVars: map[string]string{
 			"AWS_DEFAULT_REGION": region,
